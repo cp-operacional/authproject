8e3a3d594c179e3db53cf29ee74fc98f84029b74
Suporte Operacional <operacional@contactpro.com.br>
2024-09-06 16:01:33 -0300

    fix: using colors context

--- src/components/ColorsList/index.tsx
+++ src/components/ColorsList/index.tsx
@@ -1,73 +1,28 @@
 import { useEffect, useState } from 'react'
 import { Button, TextField } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete'
+
+import { AddColorPU } from '../AddColorPU'
+import { useColors } from '../../contexts/ColorsContext'
+
 import * as S from './styles'
 
-type ColorsType = {
-  id: number
-  name: string
-  year: number
-  color: string
-  pantone_value: string
-}
-
-type ColorsListType = {
-  page: number
-  per_page: number
-  total: number
-  total_pages: number
-  data: ColorsType[]
-}
-
-type ArrayColorsListType = ColorsListType[]
-
 const ColorsList = () => {
-  const [colors, setColors] = useState<ColorsType[]>([])
-  const [colorsList, setColorsList] = useState<ArrayColorsListType>([])
+  const {
+    fetchColors,
+    colorsList,
+    perPage,
+    setPerPage,
+    totalPages,
+    deleteColor
+  } = useColors()
+
   const [page, setPage] = useState(1)
-  const [perPage, setPerPage] = useState(4)
-  const [totalPages, setTotalPages] = useState(Math.ceil(12 / perPage))
   const [newPerPage, setNewPerPage] = useState<number>(perPage)
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const fetchedData: ColorsType[] = []
-        for (let i = 1; i <= totalPages; i++) {
-          const res = await fetch(`https://reqres.in/api/unknown?page=${i}`)
-          const dataJson: ColorsListType = await res.json()
-          fetchedData.push(...dataJson.data)
-        }
-        setColors(fetchedData)
-      } catch (error) {
-        console.error('Error fetching data:', error)
-      }
-    }
-
-    getData()
+    fetchColors()
   }, [])
-
-  useEffect(() => {
-    const colorsList = colors.reduce<ArrayColorsListType>(
-      (acc, color, index) => {
-        if (index % perPage === 0) {
-          acc.push({
-            page: acc.length + 1,
-            per_page: perPage,
-            total: colors.length,
-            total_pages: Math.ceil(colors.length / perPage),
-            data: [color]
-          })
-        } else {
-          acc[acc.length - 1].data.push(color)
-        }
-        return acc
-      },
-      []
-    )
-
-    setColorsList(colorsList)
-    setTotalPages(colorsList.length)
-  }, [colors, perPage])
 
   const currentPageData = colorsList.find((pageData) => pageData.page === page)
 
@@ -76,46 +31,32 @@ const ColorsList = () => {
     setNewPerPage(value)
   }
 
-  const isFirstPage = page === 1
+  const isFirstPage = page <= 1
 
-  const isLastPage = page === totalPages
+  const isLastPage = page >= totalPages
 
   const applyPerPageChange = () => {
     setPerPage(newPerPage)
-    const newTotalPages = Math.ceil(colors.length / newPerPage)
-    setTotalPages(newTotalPages)
     setPage(1)
   }
-
-  const handleAddColor = () => {
-    const newColor = {
-      id: colors.length + 1,
-      name: 'New Color',
-      year: 2021,
-      color: '#000000',
-      pantone_value: '0000'
-    }
-
-    setColors((prev) => [...prev, newColor])
-  }
-
-  useEffect(() => {
-    console.log(colors)
-  }, [colors])
 
   return (
     <section>
       <S.ButtonContainer>
-        <Button type="button" onClick={handleAddColor} variant="outlined">
-          Adicionar Cor
-        </Button>
+        <AddColorPU />
       </S.ButtonContainer>
       {currentPageData && (
         <S.List>
           {currentPageData.data.map((color) => (
             <S.ListItem key={color.id} $color={color.color}>
               <div className="content">
-                <p>{color.name}</p>
+                <div className="itemHeader">
+                  <p>{color.name}</p>
+                  <DeleteIcon
+                    className="trashIcon"
+                    onClick={() => deleteColor(color.id)}
+                  />
+                </div>
                 <p>{color.pantone_value}</p>
                 <p>{color.year}</p>
               </div>
@@ -127,7 +68,7 @@ const ColorsList = () => {
       <S.ButtonContainer>
         <Button
           type="button"
-          onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
+          onClick={() => setPage((prev) => prev - 1)}
           disabled={isFirstPage}
           variant="outlined"
         >
@@ -146,7 +87,7 @@ const ColorsList = () => {
         </Button>
         <Button
           type="button"
-          onClick={() => setPage((prev) => Math.min(prev + 1, totalPages))}
+          onClick={() => setPage((prev) => prev + 1)}
           disabled={isLastPage}
           variant="outlined"
         >
