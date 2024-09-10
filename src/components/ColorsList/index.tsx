8d12e92b5c0ae7cade6560d641e18d9401047820
Suporte Operacional <operacional@contactpro.com.br>
2024-09-10 11:43:53 -0300

    feat: implementing edit color

--- src/components/ColorsList/index.tsx
+++ src/components/ColorsList/index.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react'
-import { Button, TextField } from '@mui/material'
+import { Button, TextField, IconButton } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
+import EditIcon from '@mui/icons-material/Edit'
+import CheckIcon from '@mui/icons-material/Check'
+import CancelIcon from '@mui/icons-material/Cancel'
+import { Cancel } from '@mui/icons-material'
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward'
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward'
 
 import { AddColorPU } from '../AddColorPU'
-import { useColors } from '../../contexts/ColorsContext'
+import { ColorWithoutId, useColors } from '../../contexts/ColorsContext'
 
 import * as S from './styles'
 
@@ -17,17 +21,25 @@ const ColorsList = () => {
     setPerPage,
     totalPages,
     deleteColor,
-    moveColor
+    moveColor,
+    editColor
   } = useColors()
 
   const [page, setPage] = useState(1)
   const [newPerPage, setNewPerPage] = useState<number>(perPage)
+  const [indexColorEditing, setIndexColorEditing] = useState<number>()
+  const [newColor, setNewColor] = useState<ColorWithoutId>()
+  const [hexColorError, setHexColorError] = useState(false)
 
   useEffect(() => {
     fetchColors()
   }, [])
 
   const currentPageData = colorsList.find((pageData) => pageData.page === page)
+
+  const handlePreviousPage = () => setPage((prev) => prev - 1)
+
+  const handleNextPage = () => setPage((prev) => prev + 1)
 
   const handlePerPageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = parseInt(event.target.value, 10)
@@ -54,6 +66,65 @@ const ColorsList = () => {
     )
   }
 
+  const isEditing = (id: number) => indexColorEditing === id
+
+  const handleStartEditing = (id: number) => {
+    setIndexColorEditing(id)
+    setNewColor(currentPageData?.data.find((color) => color.id === id))
+    setHexColorError(false)
+  }
+
+  const handleEditColor = (id: number) => {
+    if (newColor) {
+      editColor(id, newColor)
+      setIndexColorEditing(undefined)
+      setNewColor(undefined)
+      setHexColorError(false)
+    }
+  }
+
+  const handleCancelEditing = () => {
+    setIndexColorEditing(undefined)
+    setNewColor(undefined)
+    setHexColorError(false)
+  }
+
+  const handleSetName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (newColor) {
+      setNewColor({ ...newColor, name: event.target.value })
+    }
+  }
+
+  const handleSetPantone = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (newColor) {
+      setNewColor({ ...newColor, pantone_value: event.target.value })
+    }
+  }
+
+  const handleSetYear = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const year = parseInt(event.target.value, 10)
+
+    if (newColor) {
+      setNewColor({ ...newColor, year: year ? year : 0 })
+    }
+  }
+
+  const handleSetColor = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+
+    const hexColorRegex = /^#([0-9A-Fa-f]{3}){1,2}$/
+
+    if (newColor) {
+      setNewColor({ ...newColor, color: value })
+    }
+
+    if (hexColorRegex.test(value)) {
+      setHexColorError(false)
+    } else {
+      setHexColorError(true)
+    }
+  }
+
   return (
     <section>
       <S.ButtonContainer>
@@ -64,35 +135,104 @@ const ColorsList = () => {
           {currentPageData.data.map((color) => (
             <S.ListItem key={color.id} $color={color.color}>
               <div className="content">
-                <div className="itemHeader">
-                  <p className="name">{color.name}</p>
-                  <DeleteIcon
-                    className="trashIcon"
-                    onClick={() => deleteColor(color.id)}
-                  />
+                <div className="colorButtonsContainer">
+                  {isEditing(color.id) ? (
+                    <>
+                      <IconButton disabled={hexColorError}>
+                        <CheckIcon
+                          className={
+                            hexColorError ? 'checkIcon disabled' : 'checkIcon'
+                          }
+                          onClick={() => handleEditColor(color.id)}
+                        />
+                      </IconButton>
+                      <IconButton>
+                        <CancelIcon
+                          className="cancelIcon"
+                          onClick={handleCancelEditing}
+                        />
+                      </IconButton>
+                    </>
+                  ) : (
+                    <>
+                      <IconButton>
+                        <EditIcon
+                          className="editIcon"
+                          onClick={() => handleStartEditing(color.id)}
+                        />
+                      </IconButton>
+                      <IconButton>
+                        <DeleteIcon
+                          className="trashIcon"
+                          onClick={() => deleteColor(color.id)}
+                        />
+                      </IconButton>
+                    </>
+                  )}
                 </div>
-                <p className="pantone">{color.pantone_value}</p>
-                <p className="year">{color.year}</p>
+                <div className="colorDescription">
+                  {isEditing(color.id) ? (
+                    <>
+                      <input
+                        type="text"
+                        className="nameInput"
+                        value={newColor?.name}
+                        onChange={handleSetName}
+                      />
+                      <input
+                        type="text"
+                        className="pantoneInput"
+                        value={newColor?.pantone_value}
+                        onChange={handleSetPantone}
+                      />
+                      <input
+                        type="text"
+                        className="yearInput"
+                        value={newColor?.year}
+                        onChange={handleSetYear}
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <div className="itemHeader">
+                        <p className="name">{color.name}</p>
+                      </div>
+                      <p className="pantone">{color.pantone_value}</p>
+                      <p className="year">{color.year}</p>
+                    </>
+                  )}
+                </div>
               </div>
               <div className="right">
-                <span className="color" />
+                <span className="color">
+                  {isEditing(color.id) && (
+                    <input
+                      type="text"
+                      className={
+                        hexColorError ? 'hexColorInput error' : 'hexColorInput'
+                      }
+                      value={newColor?.color}
+                      onChange={handleSetColor}
+                    />
+                  )}
+                </span>
                 <div className="movingButtonContainer">
-                  <Button
+                  <IconButton
                     className="movingButton"
                     onClick={() => moveColor(color.id, 'up')}
                     disabled={isFirstColorId(color.id)}
                     color="primary"
                   >
                     <ArrowUpwardIcon />
-                  </Button>
-                  <Button
+                  </IconButton>
+                  <IconButton
                     className="movingButton"
                     onClick={() => moveColor(color.id, 'down')}
                     disabled={isLastColorId(color.id)}
                     color="primary"
                   >
                     <ArrowDownwardIcon />
-                  </Button>
+                  </IconButton>
                 </div>
               </div>
             </S.ListItem>
@@ -102,7 +242,7 @@ const ColorsList = () => {
       <S.ButtonContainer>
         <Button
           type="button"
-          onClick={() => setPage((prev) => prev - 1)}
+          onClick={handlePreviousPage}
           disabled={isFirstPage}
           variant="outlined"
         >
@@ -121,7 +261,7 @@ const ColorsList = () => {
         </Button>
         <Button
           type="button"
-          onClick={() => setPage((prev) => prev + 1)}
+          onClick={handleNextPage}
           disabled={isLastPage}
           variant="outlined"
         >
