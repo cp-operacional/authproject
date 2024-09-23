20ae35c8d4deae52f3a803658df251141792fbca
cp-operacional <operacional@contactpro.com.br>
2024-09-23 14:18:33 -0300

    feat: using updateAvatar on component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,19 +1,45 @@
+import { useRef } from 'react'
 import { Button } from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit'
 
 import { useAuth } from '../../contexts/AuthContext'
 
 import * as S from './styles'
 
 const Sidebar = () => {
-  const { user, logout } = useAuth()
+  const { user, logout, updateAvatar } = useAuth()
+  const fileInputRef = useRef<HTMLInputElement>(null)
+
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const file = event.target.files?.[0]
+    if (file) {
+      try {
+        await updateAvatar(file)
+      } catch (error) {
+        console.error('Erro ao atualizar o avatar:', error)
+      }
+    }
+  }
 
   return (
     <S.Container>
       <div className="userData">
-        <img
-          className="avatar"
-          src={user?.avatar}
-          alt={`${user?.first_name} ${user?.last_name} avatar`}
+        <S.AvatarContainer onClick={() => fileInputRef.current?.click()}>
+          <S.Avatar
+            src={user?.avatar}
+            alt={`${user?.first_name} ${user?.last_name} avatar`}
+          />
+          <div className="avatarOverlay">
+            <EditIcon className="editIcon" />
+          </div>
+        </S.AvatarContainer>
+        <input
+          type="file"
+          ref={fileInputRef}
+          style={{ display: 'none' }}
+          onChange={handleFileChange} // Adicione esta linha
         />
         <p className="email">{user?.email}</p>
         <p>{`${user?.first_name} ${user?.last_name}`}</p>
